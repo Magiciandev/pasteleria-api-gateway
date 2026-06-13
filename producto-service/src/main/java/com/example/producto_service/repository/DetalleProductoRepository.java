07cf592687ef06227268baaaef2f164e656cdba4
Max <map.herrera@duocuc.cl>
2026-06-13 00:07:01 -0400

    clean: remover archivos duplicados accidentales

--- producto-service/src/main/java/com/example/producto_service/repository/DetalleProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.producto_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.producto_service.model.DetalleProducto;
-
-@Repository
-public interface DetalleProductoRepository extends JpaRepository<DetalleProducto, Long> {
-}
